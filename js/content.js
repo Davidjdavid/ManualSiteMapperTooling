22c11f3434f770a22b26c4707f5d55afbee7611f
davidjdavid <dave.j.hazelwood@gmail.com>
2025-11-09 20:26:27 -0600

    Basic link clicking and storing added

--- js/content.js
+++ js/content.js
@@ -1,5 +1,21 @@
+// Variable to store all of the links we grab.
+let capturedLinks = [];
+
 document.addEventListener("click", logKey);
 
 function logKey(e) {
-    console.log(`"The alt key is pressed: "${e.altKey}`);
+    const linkElement = event.target.closest('a');
+    if (linkElement) { // This can be null if a link isnt actually clicked on
+    
+    // Stop the browser from navigating to the link.
+    event.preventDefault();
+    // Get the URL from the link's href attribute.
+    const url = linkElement.href;
+    // Add the URL to the captured links.
+    capturedLinks.push(url); 
+    // Convert the array to a JSON string. NOTE: This is not what we need it to do. We want this to be hierarchy
+    let linksAsJSON = JSON.stringify(capturedLinks);
+    console.log(linksAsJSON); // Delete later
+
+    }
 }
