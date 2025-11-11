bb369cac4f3eb1ad8772ebd97396eb5de3a8d913
davidjdavid <dave.j.hazelwood@gmail.com>
2025-11-10 21:49:27 -0600

    Updated the script so that the excel row classes are pushed into array that will be JSON exported later

--- js/content.js
+++ js/content.js
@@ -22,17 +22,15 @@ class ExcelRow {
 function logKey(e) {
     const linkElement = event.target.closest('a');
     if (linkElement) { // This can be null if a link isnt actually clicked on
-    
-    // Stop the browser from navigating to the link.
-    event.preventDefault();
-    // Get the URL from the link's href attribute.
-    const url = linkElement.href;
-    // Add the URL to the captured links.
-    capturedLinks.push(url); 
-    // Convert the array to a JSON string. NOTE: This is not what we need it to do. We want this to be hierarchy
-    let linksAsJSON = JSON.stringify(capturedLinks);
-    console.log(linksAsJSON); // Delete later
-    let testRow = new ExcelRow({})
-    testRow.printCol1();
+        let testRow = new ExcelRow({})
+        // Stop the browser from navigating to the link.
+        event.preventDefault();
+        // Get the URL from the link's href attribute.
+        testRow._col1 = linkElement.href;
+        // Add the URL to the captured links.
+        capturedLinks.push(testRow); 
+        // Convert the array to a JSON string. NOTE: This is not what we need it to do. We want this to be hierarchy
+        let linksAsJSON = JSON.stringify(capturedLinks);
+        console.log(linksAsJSON); // Delete later
     }
 }
