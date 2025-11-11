b81605bd3a5eaa61009f556c953c7356fb96c5c9
davidjdavid <dave.j.hazelwood@gmail.com>
2025-11-10 19:02:34 -0600

    Added the constructor class for the Excel row to hold columns 1 through 5. Also a function to console log the value of column one for testing.

--- js/content.js
+++ js/content.js
@@ -3,6 +3,22 @@ let capturedLinks = [];
 
 document.addEventListener("click", logKey);
 
+// Class that is used to call on when a new Excel row is used
+class ExcelRow {
+    constructor(params) {
+        this._col1 = params.col1 || "Empty"
+        this._col2 = params.col1 || "Empty"
+        this._col3 = params.col1 || "Empty"
+        this._col4 = params.col1 || "Empty"
+        this._col5 = params.col1 || "Empty"
+    }
+
+    printCol1() {
+        console.log(this._col1)
+    }
+}
+
+
 function logKey(e) {
     const linkElement = event.target.closest('a');
     if (linkElement) { // This can be null if a link isnt actually clicked on
@@ -16,6 +32,7 @@ function logKey(e) {
     // Convert the array to a JSON string. NOTE: This is not what we need it to do. We want this to be hierarchy
     let linksAsJSON = JSON.stringify(capturedLinks);
     console.log(linksAsJSON); // Delete later
-
+    let testRow = new ExcelRow({})
+    testRow.printCol1();
     }
 }
