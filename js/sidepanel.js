15d212ac4cdda225d01772336974711e72955a15
David Hazelwood <dave.j.hazelwood@gmail.com>
2025-11-17 17:37:51 -0600

    Update sidepanel.js

--- js/sidepanel.js
+++ js/sidepanel.js
@@ -4,23 +4,22 @@ console.log('sidepanel.js');
 let columnSelect = document.getElementById("toggleCheckbox");
 console.log(columnSelect);
 
-//Changes the current column we are adding pages to whatever depth the user clicks on their keyboard
-document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "1":
-            // Fixed typo: currnetColumn -> currentColumn
-            return currentColumn = "1";
-        case "2":
-            return currentColumn = "2";
-        case "3":
-            return currentColumn = "3";
-        case "4":
-            return currentColumn = "4";
-        case "5":
-            return currentColumn = "5";
-    }
+document.querySelectorAll('input[name="column"]').forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+        chrome.runtime.sendMessage({ 
+            action: "updateColumn", 
+            column: event.target.value 
+        });
+    });
 });
 
 document.getElementById("exportBtn").addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: "doSomething"});
+    chrome.runtime.sendMessage({ action: "exportButton"});
+});
+
+document.getElementById("toggle").addEventListener('change', (event) => {
+    chrome.runtime.sendMessage({
+        action: "toggle",
+        toggle: event.target.checked
+    })
 });
